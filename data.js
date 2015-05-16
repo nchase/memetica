ee63796dc2672e2af55be9c6a995ee7d7a7fa6e3
Noah Chase <nchase@gmail.com>
2015-05-16 12:42:32 -0400

    initial commit

---
+++ data.js
@@ -0,0 +1,6 @@
+module.exports = {
+  frames: [],
+  styles: [
+    'steel_moss'
+  ]
+};
---
+++ index.js
@@ -0,0 +1,40 @@
+require('babel/register');
+
+var express = require('express');
+var app = express();
+var sass = require('node-sass');
+var autoprefixer = require('autoprefixer-core');
+
+app.engine('html', require('ejs').renderFile);
+
+app.set('view engine', 'html');
+app.set('views', process.cwd() + '/src/');
+
+app.locals.require = require;
+app.locals.marked = require('marked');
+
+app.get('/', function(request, response){
+  response.render('index', {
+    data: require('./data')
+  });
+});
+
+app.get('/base.css', function(request, response) {
+  response.set('Content-Type', 'text/css');
+  response.send(autoprefixer.process(sass.renderSync({
+    file: process.cwd() + '/src/assets/stylesheets/base.scss'
+  }).css).css);
+});
+
+app.use(express.static(__dirname + '/src/assets'));
+
+var invokeArgs = process.argv.slice(2).join(' ');
+
+var listenPort = process.env.PORT ||
+  invokeArgs.match(/(?:port=)([0-9]+)\s?/) &&
+  invokeArgs.match(/(?:port=)([0-9]+)\s?/)[1] ||
+  3000;
+
+app.listen(listenPort);
+
+console.log('http server listening on', listenPort);

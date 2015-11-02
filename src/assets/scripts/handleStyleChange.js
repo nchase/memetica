74f576e56c92d9f33a7ea1656d5b38366de7842f
Noah Chase <nchase@gmail.com>
2015-11-02 18:07:44 -0500

    add style randomizer if user hasn't picked a style.

--- src/assets/scripts/handleStyleChange.js
+++ src/assets/scripts/handleStyleChange.js
@@ -1,12 +1,28 @@
-function handleStyleChange(event) {
+function handleStyleChange(event, options) {
   document.getElementById('style').setAttribute('href', event.target.value + '.css');
-  document.cookie = "style=" + event.target.value;
+
+  if (!event.transient) {
+    document.cookie = "style=" + event.target.value;
+  }
 }
 
 document.querySelector('.ctl--styleChanger').addEventListener('change', handleStyleChange);
 
 document.addEventListener('DOMContentLoaded', function() {
-  document.querySelector('.ctl--styleChanger').value =
-    document.cookie.match(/style=(.+);?/) && document.cookie.match(/style=(.+);?/)[1];
-  document.querySelector('.ctl--styleChanger').dispatchEvent(new Event('change'));
+  var cookieValue = document.cookie.match(/style=(.+);?/) && document.cookie.match(/style=(.+);?/)[1];
+  var options;
+  var evt = new Event('change');
+  evt.transient = false;
+
+  if (!cookieValue) {
+    evt.transient = true;
+    options = document.querySelectorAll('.ctl--styleChanger option');
+    cookieValue = options[Math.floor(Math.random() * (options.length + 1))].value
+
+    document.querySelector('.ctl--styleChanger').value = cookieValue;
+    return document.querySelector('.ctl--styleChanger').dispatchEvent(evt);
+  }
+
+  document.querySelector('.ctl--styleChanger').value = cookieValue;
+  document.querySelector('.ctl--styleChanger').dispatchEvent(evt);
 });

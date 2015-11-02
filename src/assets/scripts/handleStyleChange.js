9a5caa4cea8adafcd8ad031c56f910ea9a82db67
Noah Chase <nchase@gmail.com>
2015-11-02 18:32:33 -0500

    resurrect query string cookies and fix random
    [random was evaluating one-index-too-high]

--- src/assets/scripts/handleStyleChange.js
+++ src/assets/scripts/handleStyleChange.js
@@ -9,15 +9,26 @@ function handleStyleChange(event, options) {
 document.querySelector('.ctl--styleChanger').addEventListener('change', handleStyleChange);
 
 document.addEventListener('DOMContentLoaded', function() {
-  var cookieValue = document.cookie.match(/style=(.+);?/) && document.cookie.match(/style=(.+);?/)[1];
-  var options;
+  var cookieValue = window.location.search.match(/(.*?)=([^\&]*)&?/) &&
+    window.location.search.match(/(.*?)=([^\&]*)&?/)[2];
+
+  if (cookieValue) {
+    cookieValue = 'src/assets/stylesheets/' + cookieValue;
+  }
+
   var evt = new Event('change');
   evt.transient = false;
+  var options;
+
+  if (!cookieValue) {
+    cookieValue = document.cookie.match(/style=(.+);?/) &&
+      document.cookie.match(/style=(.+);?/)[1];
+  }
 
   if (!cookieValue) {
     evt.transient = true;
     options = document.querySelectorAll('.ctl--styleChanger option');
-    cookieValue = options[Math.floor(Math.random() * (options.length + 1))].value
+    cookieValue = options[Math.floor(Math.random() * options.length)].value
 
     document.querySelector('.ctl--styleChanger').value = cookieValue;
     return document.querySelector('.ctl--styleChanger').dispatchEvent(evt);
